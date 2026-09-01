e9ef15ecaf911a813696089c3f9b21d639d3c2b8
Killah <jmonroyc2100@gmail.com>
2026-09-01 05:42:30 -0600

    Agregar middleware CORS para permitir solicitudes desde cualquier origen

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 from src.app.endpoints import pdf_upload
 from src.app.endpoints import query_data
 from services.db_conection import init_db, engine
@@ -13,6 +14,14 @@ async def lifespan(app: fastapi.FastAPI):
 
 
 app = fastapi.FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(pdf_upload.router, prefix="/pdf-upload")
 app.include_router(query_data.router, prefix="/data")
